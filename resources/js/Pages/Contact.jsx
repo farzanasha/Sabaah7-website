a27ecf74ef45f9677dbc0a9a3bf38a6ffc65bd56
Nurfarzana <farzanashashakvpd@gmail.com>
2026-04-25 22:08:40 +0800

    tambah contact dzul n prabu

--- resources/js/Pages/Contact.jsx
+++ resources/js/Pages/Contact.jsx
@@ -36,12 +36,15 @@ export default function Contact() {
 
                             <div>
                                 <p className="font-semibold">📞 Phone</p>
-                                <p>+60 12-345 6789</p>
+                                <p>+60 11-1119 0377 (En. Dzulqarnain Adzmi)</p>
+                                <p>+60 11-6513 9295 (En. Prabu)</p>
                             </div>
 
                             <div>
                                 <p className="font-semibold">📧 Email</p>
-                                <p>info@sabaah7.com</p>
+                                <p>idzulqarnain.adzmi@sabaah7.com</p>
+                                <p>prabu.daymudoo@sabaah7.com</p>
+                            
                             </div>
 
                             <div>
